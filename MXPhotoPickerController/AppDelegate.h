3d96a1825f430eff2eff7d98aab48b4cfe198af9
NingmengDev <ningmengdev@163.com>
2016-05-01 13:57:32 +0800

    Initial Commit

---
+++ MXPhotoPickerController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MXPhotoPickerController
+//
+//  Created by 韦纯航 on 16/5/1.
+//  Copyright © 2016年 韦纯航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
